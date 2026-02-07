c96bb8f51f1666de217508a63153e861fc2c93b7
AbdulHamid Merii <AH_Merii@outlook.com>
2026-02-07 05:35:54 +0100

    Revert "fix: process icon paths one at a time to avoid reordering bugs"
    This reverts commit acc27074c84c9a60db7b049ce1ad851628646b8b.

--- tests/search_directory/icons_enabled.fish
+++ tests/search_directory/icons_enabled.fish
@@ -1,16 +1,10 @@
-# Mock icon command that reverses argument order (simulates eza's default sorting)
-# This catches the batch-reordering bug: if _fzf_add_icons passes multiple paths
-# at once, the decorated↔original pairing breaks when the command reorders output.
-function reordering_icon_cmd
-    set -l reversed
-    for i in (seq (count $argv) -1 1)
-        set -a reversed $argv[$i]
-    end
-    for arg in $reversed
+# Define a mock icon command that prefixes each path with an icon
+function my_icon_cmd
+    for arg in $argv
         echo "F $arg"
     end
 end
-set fzf_icon_cmd reordering_icon_cmd
+set fzf_icon_cmd my_icon_cmd
 
 mock commandline --current-token "echo tests/_resources/multi word dir/"
 mock commandline "--current-token --replace --" "echo \$argv"
@@ -19,4 +13,4 @@ set --export --append FZF_DEFAULT_OPTS "--filter=''"
 
 set actual (_fzf_search_directory)
 set expected "'tests/_resources/multi word dir/file 1.txt' 'tests/_resources/multi word dir/file 2.txt'"
-@test "returns original paths when icon command reorders output" "$actual" = $expected
+@test "returns original paths when icon mode is enabled" "$actual" = $expected
